a25bd4cfdd7a4523858e8568dd6fd9ce3b8ff900
petronemilio <emiliopetronem@gmail.com>
2022-05-21 11:41:53 -0500

    Script to download the database form try and read the xlsx file. Column names properly renamed and transformed

--- scripts/DataBasePrep.R
+++ scripts/DataBasePrep.R
@@ -5,25 +5,31 @@ R.version
 #Load useful libraries
 library(ggplot2)
 library(dplyr)
+library(readxl)
 library(emmeans) #library to get means of linear models with more than one categorical variable
-
-#Load the database
-#cell <- read.csv("../conduitDiameter/Data/VesselDiameterVesselWall.csv",row.names = 1)
-cell <- read.csv("data/VesselDiameterVesselWall.csv",row.names = 1)
+#To download data from TRY database you need to register. Then copy the url of the database
+download.file('https://www.try-db.org/tmpdnld/Try2022521181315815_VesselDiameterVesselWallThickness.xlsx'
+              ,'data/VesselDiameterVesselWallThickness.xlsx')
+#You can open the xlsx file and save it as csv or use read.xls comand
+cell<-read_xlsx("data/VesselDiameterVesselWallThickness.xlsx")
+summary(cell)
 #Change column names
 names(cell)[1:8] <- c("ord", "fam", "gen","sp","auth", "col","ncol","o")
-names(cell)[names(cell) == "VDFinal.Vessel.diameter.interpreted"] <- "vd"
-names(cell)[names(cell) == "VWTFINAL.Vessel.wall.thickness.draw.micrometers"] <- "vwt"
-names(cell)[names(cell)=="FINAL.scalariform.not" ] <- "simp.scal"
-names(cell)[names(cell)=="ITEWTFINAL..wall.thick.final.micrometers" ] <- "ite.wt"
-names(cell)[names(cell)=="ITE.Final" ] <- "ite.type"
-names(cell)[names(cell)=="self.non"] <- "self"
-names(cell)[names(cell)=="DWT..Double.wall.thickness..um."  ] <- "dwt"
+names(cell)[names(cell) == "Vessel.diameter.micrometers.curated"] <- "vd"
+names(cell)[names(cell) == "Vessel.wall.thickness.micrometers.curated"] <- "vwt"
+names(cell)[names(cell)=="perforation.plate.type.curated"] <- "simp.scal"
+names(cell)[names(cell)=="Imperforate.tracheary.element.wall.thickness.micrometers.curated"] <- "ite.wt"
+names(cell)[names(cell)=="Imperforate.tracheary.element.type.curated"] <- "ite.type"
+names(cell)[names(cell)=="Vessels.group.curated"] <- "vg"
+names(cell)[names(cell)=="Imperforate.tracheary.element.diametermicrometers.curated"] <- "ite.d"
+#names(cell)[names(cell)=="self.supporting"
+names(cell)[names(cell)=="DWT..Double.wall.thickness..um."] <- "dwt"
 ####numeric
 cell$vd <- as.numeric(cell$vd)
 cell$vwt <- as.numeric(cell$vwt)
 cell$ite.wt <- as.numeric(cell$ite.wt)
 cell$dwt <- as.numeric(cell$dwt)
+cell$vg <- as.numeric(cell$vg)
 cell$dwt.conditional <- ifelse(cell$vg > 1.5, cell$vwt*2, cell$vwt+cell$ite.wt)
 cell$spe <- paste(cell$gen, cell$sp, sep=" ")
 ##
@@ -31,8 +37,8 @@ cell$conductive <- cell$ite.type == "tracheids" #asign logical values to conduct
 cell$conductive <- as.numeric(cell$conductive) #CONVERT TO NUMERIC 0= libriform and fiber tracheids
 cell$conductive[cell$conductive==0] <- "Non-conductive"  #0nonconductive
 cell$conductive[cell$conductive==1] <- "Conductive" #1 conductive
-cell$WDratio <- round((cell$vwt/cell$vd), 2) ###razón grosor pared entre diametro 
-cell$DWDratio <- round((cell$dwt/cell$vd), 2) ###razón grosor pared doble entre diametro
+cell$WDratio <- round((cell$vwt/cell$vd), 2) ###ratio between vessel wall thickness and vessel diameter 
+cell$DWDratio <- round((cell$dwt/cell$vd), 2) ###ratio double wall thickness and vessel diameter
 
 ###Temporal copy from base
 cell.temp.subseted <- cell
